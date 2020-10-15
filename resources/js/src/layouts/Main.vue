b68f39eba0925340ff0571e30843630615a3194f
Adrian López <carlos.lazpeitia@alumnos.udg.mx>
2020-10-15 00:36:20 -0500

    Se crea layout de vue

---
+++ resources/js/src/layouts/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <form-notification />
+  </div>
+</template>
+<script>
+import FormNotification from "../catalogs/FormNotification/FormNotification";
+export default {
+  components: {
+    FormNotification,
+  },
+  metaInfo() {
+    return { title: "Inicio" };
+  },
+};
+</script>
